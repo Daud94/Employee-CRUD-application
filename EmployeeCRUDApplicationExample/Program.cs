fd6296b9e4a54fd4fa08f62bb3471c67096e73c2
Daud Olarewaju <olarewajuyahyadaud@gmail.com>
2024-06-23 17:24:16 +0100

    Employee class

---
+++ EmployeeCRUDApplicationExample/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace EmployeeCRUDApplicationExample;
+
+class Program
+{
+    public class Employee
+    {
+        public int Id;
+        public string FirstName;
+        public string LastName;
+        public decimal AnnualAsalary;
+        public char Gender;
+        public bool IsManager;
+    }
+    
+    static void Main(string[] args)
+    {
+        var employee = new Employee
+        {
+            Id = 1,
+            FirstName = "John",
+            LastName = "Doe",
+            Gender = 'M',
+            AnnualAsalary = 5000,
+            IsManager = true
+        };
+
+        Console.WriteLine($"Annual salary before update{employee.AnnualAsalary}");
+        UpdateAnnualSalary(employee);
+        Console.WriteLine($"Annual salary after update{employee.AnnualAsalary}");
+
+    }
+
+    public static void UpdateAnnualSalary(Employee employee)
+    {
+        employee.AnnualAsalary = 100000;
+        
+    }
+}
---
+++ EmployeeComponent/Data/Employee.cs
@@ -0,0 +1,69 @@
+﻿namespace EmployeeComponent.Data;
+
+public class Employee
+{
+    private static int _nextId = 0;
+    private int _id = 0;
+
+    private string _firstName = string.Empty;
+    private string _lastName = string.Empty;
+    private decimal _annualSalary = 0;
+    private char _gender = '\0';
+    private bool _isManager = false;
+
+
+    public int Id => _id;
+
+    public string FirstName
+    {
+        get => _firstName;
+        set => _firstName = value;
+    }
+
+    public string LastName
+    {
+        get => _lastName;
+        set => _lastName = value;
+    }
+
+    public decimal AnnualSalary
+    {
+        get => _annualSalary;
+        set => _annualSalary = value;
+    }
+
+    public char Gender
+    {
+        get => _gender;
+        set => _gender = value;
+    }
+
+    public bool IsManager
+    {
+        get => _isManager;
+        set => _isManager = value;
+    }
+
+    public Employee()
+    {
+        _nextId++;
+        _id = _nextId;
+    }
+
+    public Employee(string firstName, string lastName, decimal annualSalary, char gender, bool isManager): this()
+    {
+        _firstName = firstName;
+        _lastName = lastName;
+        _annualSalary = annualSalary;
+        _gender = gender;
+        _isManager = isManager;
+    }
+
+    public string GetEmployeeInformation()
+    {
+        string employeeInformation =
+            $"{Id.ToString().PadRight(6)}\t{FirstName.PadRight(15)}\t{LastName.PadRight(15)}\t{AnnualSalary.ToString().PadRight(15)}\t{Gender.ToString().PadRight(6)}\t{IsManager.ToString().PadRight(7)}";
+        return employeeInformation;
+        
+    }
+}
